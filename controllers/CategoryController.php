cfe533fc294ec0e7ccca9abcd4bcc141ec283e49
Igor Tarasenko <igtarako@gmail.com>
2018-05-03 21:15:12 +0700

    add AFC for category controller

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -8,6 +8,7 @@ use app\models\CategorySearchModel;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 use yii\helpers\ArrayHelper;
 
 /**
@@ -27,6 +28,17 @@ class CategoryController extends Controller
                     'delete' => ['POST'],
                 ],
             ],
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['create', 'update', 'delete'],
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'actions' => ['create', 'update', 'delete'],
+                        'roles' => ['@']
+                    ]
+                ]
+            ]
         ];
     }
 
