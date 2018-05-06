1fbbfec541453a1c884ab8062263c0fad7a2238c
Igor Tarasenko <igtarako@gmail.com>
2018-05-06 23:06:43 +0700

    add html to chart view

--- views/account/chart.php
+++ views/account/chart.php
@@ -1,5 +1,14 @@
 <?php
+
 use dosamigos\chartjs\ChartJs;
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+
+$this->title = 'Accounts Chart';
+$this->params['breadcrumbs'][] = ['label' => 'Accounts', 'url' => ['index']];
+$this->params['breadcrumbs'][] = 'Chart';
+
 
 function createDatasetFromOperation($account, $operation){
     list($r, $g, $b) = generateRGB();
@@ -34,11 +43,18 @@ foreach($operationsByAccount as $account => $operation){
     $data['datasets'][] = createDatasetFromOperation($account, $operation);
 }
 
-echo ChartJs::widget([
-    'type' => 'line',
-    'options' => [
-        'height' => 200,
-        'width' => 300
-    ],
-    'data' => $data
-]);
+?>
+<div class="account-chart">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= ChartJs::widget([
+        'type' => 'line',
+        'options' => [
+            'height' => 200,
+            'width' => 300
+        ],
+        'data' => $data
+    ]) ?>
+
+</div>
