0f48445ba1aa4df1c1b6f843ff6b781140f486ae
Igor Tarasenko <igtarako@gmail.com>
2018-05-06 23:00:31 +0700

    add chart render  from arrays of operation sets

--- views/account/chart.php
+++ views/account/chart.php
@@ -1,35 +1,44 @@
 <?php
 use dosamigos\chartjs\ChartJs;
 
+function createDatasetFromOperation($account, $operation){
+    list($r, $g, $b) = generateRGB();
+    $dataset = [];
+    $dataset['label'] = $account;
+    $dataset['data'] = array_values($operation);
+    $dataset['pointBorderColor'] = "#fff";
+    $dataset['pointHoverBackgroundColor'] = "#fff";
+    $dataset['borderColor'] = "rgba($r,$g,$b,1)";
+    $dataset['pointHoverBorderColor'] = "rgba($r,$g,$b,1)";
+    $dataset['pointBackgroundColor'] = "rgba($r,$g,$b,1)";
+    $dataset['backgroundColor'] = "rgba($r,$g,$b,0.2)";
+
+    return $dataset;
+}
+
+function generateRGB(){
+    $r = rand(0,255);
+    $g = rand(0,255);
+    $b = rand(0,255);
+    return [$r, $g, $b];
+}
+
+$data = [
+        'labels' => [],
+        'datasets'=> [],
+    ];
+
+$data['labels'] = array_keys(current($operationsByAccount));
+
+foreach($operationsByAccount as $account => $operation){
+    $data['datasets'][] = createDatasetFromOperation($account, $operation);
+}
+
 echo ChartJs::widget([
     'type' => 'line',
     'options' => [
         'height' => 200,
         'width' => 300
     ],
-    'data' => [
-        'labels' => ["January", "February", "March", "April", "May", "June", "July"],
-        'datasets' => [
-            [
-                'label' => "My First dataset",
-                'backgroundColor' => "rgba(179,181,198,0.2)",
-                'borderColor' => "rgba(179,181,198,1)",
-                'pointBackgroundColor' => "rgba(179,181,198,1)",
-                'pointBorderColor' => "#fff",
-                'pointHoverBackgroundColor' => "#fff",
-                'pointHoverBorderColor' => "rgba(179,181,198,1)",
-                'data' => [65, 59, 90, 81, 56, 55, 40]
-            ],
-            [
-                'label' => "My Second dataset",
-                'backgroundColor' => "rgba(255,99,132,0.2)",
-                'borderColor' => "rgba(255,99,132,1)",
-                'pointBackgroundColor' => "rgba(255,99,132,1)",
-                'pointBorderColor' => "#fff",
-                'pointHoverBackgroundColor' => "#fff",
-                'pointHoverBorderColor' => "rgba(255,99,132,1)",
-                'data' => [28, 48, 40, 19, 96, 27, 100]
-            ]
-        ]
-    ]
+    'data' => $data
 ]);
